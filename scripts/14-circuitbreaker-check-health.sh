9ae9c12fc09d0aacca25c14977ffaa0aa4ff52f4
Shuichi Machida <smachida@macbook.local>
2022-06-09 17:47:19 +0900

    updated

--- scripts/14-circuitbreaker-check-health.sh
+++ scripts/14-circuitbreaker-check-health.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it --network=msdemo10resiliency_my-network alpine wget product-composite:8080/actuator/health -qO - | jq -r .components.circuitBreakers.details.product.details.state
+docker run --rm -it --network=ms-demo-10-resiliency_my-network alpine wget product-composite:8080/actuator/health -qO - | jq -r .components.circuitBreakers.details.product.details.state
